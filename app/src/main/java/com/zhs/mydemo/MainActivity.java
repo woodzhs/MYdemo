84e167c5c0813cc976bf6303e54559cf1e619e0d
zhenghaisong <1093122410@qq.com>
2016-10-03 00:56:54 +0800

    First commit

---
+++ app/src/main/java/com/zhs/mydemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zhs.mydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.graphics.Color;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
